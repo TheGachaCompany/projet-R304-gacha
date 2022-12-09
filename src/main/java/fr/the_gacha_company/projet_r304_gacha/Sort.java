5aad07e7436f2f4d84bf397c3b6405c53b2eddc4
thibaultDP <thibault.DE-PERMENTIER@etu.univ-amu.fr>
2022-12-09 17:28:33 +0100

    add lambda everywhere

--- src/main/java/fr/the_gacha_company/projet_r304_gacha/Sort.java
+++ src/main/java/fr/the_gacha_company/projet_r304_gacha/Sort.java
@@ -1,4 +1,37 @@
 package fr.the_gacha_company.projet_r304_gacha;
 
+import fr.the_gacha_company.projet_r304_gacha.hero.Hero;
+
+import java.util.*;
+
 public class Sort {
+
+    public static Comparator<Hero> nom = (Hero h1, Hero h2) -> h1.getName().compareToIgnoreCase(h2.getName());
+
+    public static Comparator<Hero> lvl = Comparator.comparingInt((Hero h) -> h.getStat().getLevel());
+
+    public static Comparator<Hero> atk = Comparator.comparingDouble((Hero h) -> h.getStat().getAttack());
+
+
+
+
+
+    public static void sortByLambda(Set<Hero> aSet,Comparator<Hero> comparator){
+        List<Hero> aList = new ArrayList<>(aSet);
+        int left = 0;
+        int right = 1;
+        int i = aList.size();
+        while ( i >= 1) {
+            while (right < i) {
+                if (comparator.compare(aList.get(left),aList.get(right))==1) {
+                    Collections.swap(aList, left, right);
+                }
+                ++left;
+                ++right;
+            }
+            --i;
+        }
+    }
+
+
 }
