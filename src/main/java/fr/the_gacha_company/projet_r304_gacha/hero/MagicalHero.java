ea8fc658f5e1bf8ba2c63ff39005168c05d0d621
Romain <Romain CLEMENT>
2022-12-11 14:05:48 +0100

    Type correction for concrete race

--- src/main/java/fr/the_gacha_company/projet_r304_gacha/hero/MagicalHero.java
+++ src/main/java/fr/the_gacha_company/projet_r304_gacha/hero/MagicalHero.java
@@ -1,10 +1,10 @@
 package fr.the_gacha_company.projet_r304_gacha.hero;
 
-import fr.the_gacha_company.projet_r304_gacha.hero.races.Race;
+import fr.the_gacha_company.projet_r304_gacha.hero.races.MagicalRace;
 
 public class MagicalHero extends Hero {
 
-    public MagicalHero(String name, Race race, Gender gender, Rarity rarity, String lore, Stat stat) {
+    public MagicalHero(String name, MagicalRace race, Gender gender, Rarity rarity, String lore, Stat stat) {
         super(name, race, gender, rarity, lore, stat);
     }
 }
--- src/main/java/fr/the_gacha_company/projet_r304_gacha/hero/PhysicalHero.java
+++ src/main/java/fr/the_gacha_company/projet_r304_gacha/hero/PhysicalHero.java
@@ -1,10 +1,10 @@
 package fr.the_gacha_company.projet_r304_gacha.hero;
 
-import fr.the_gacha_company.projet_r304_gacha.hero.races.Race;
+import fr.the_gacha_company.projet_r304_gacha.hero.races.PhysicalRace;
 
 public class PhysicalHero extends Hero {
 
-    public PhysicalHero(String name, Race race, Gender gender, Rarity rarity, String lore, Stat stat) {
+    public PhysicalHero(String name, PhysicalRace race, Gender gender, Rarity rarity, String lore, Stat stat) {
         super(name, race, gender, rarity, lore, stat);
     }
 }
