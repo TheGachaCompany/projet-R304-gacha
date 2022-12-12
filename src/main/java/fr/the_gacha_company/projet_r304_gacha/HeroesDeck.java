af40fe3d9b43312f6a68dd957b115f1d5cfbde30
Romain <Romain CLEMENT>
2022-12-12 14:05:58 +0100

    Using ArrayList and not HashSet to have an index to select heroes

--- src/main/java/fr/the_gacha_company/projet_r304_gacha/HeroesDeck.java
+++ src/main/java/fr/the_gacha_company/projet_r304_gacha/HeroesDeck.java
@@ -2,15 +2,18 @@ package fr.the_gacha_company.projet_r304_gacha;
 
 import fr.the_gacha_company.projet_r304_gacha.hero.Hero;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 
-public class HeroesDeck extends HashSet<Hero> {
+public class HeroesDeck extends ArrayList<Hero> {
 
     @Override
     public boolean add(Hero hero) {
-        if (!super.add(hero))
+        if (!contains(hero))
             // if hero is already unlocked
             hero.getStat().increment_level();
+        else
+            super.add(hero);
         return true;
     }
+
 }
