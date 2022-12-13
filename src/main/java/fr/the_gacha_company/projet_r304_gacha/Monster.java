2851dfb423943873c22b19eb0168bec9b4fc267e
Romain CLEMENT <rc2301@orange.fr>
2022-12-13 20:54:54 +0100

    Changing Monster's stats

--- src/main/java/fr/the_gacha_company/projet_r304_gacha/Monster.java
+++ src/main/java/fr/the_gacha_company/projet_r304_gacha/Monster.java
@@ -3,15 +3,15 @@ package fr.the_gacha_company.projet_r304_gacha;
 public class Monster extends Character {
 
     public static Monster createMonster() {
-        return new Monster(new Stat(Main.rand.nextInt(15, 50), Main.rand.nextInt(4, 15),
-                Main.rand.nextFloat(0.1f, 0.4f), Main.rand.nextInt(15, 40)));
+        return new Monster(new Stat(Main.rand.nextInt(15, 50), Main.rand.nextInt(3, 15),
+                Main.rand.nextFloat(0.1f, 0.4f), Main.rand.nextInt(15, 35)));
     }
 
     private final int goldValue;
 
     private Monster(Stat stat) {
         super(stat);
-        goldValue = Math.round((stat.getHp_max() + stat.getAttack() + stat.getSpeed()) * (1+stat.getDefense()));
+        goldValue = Math.round((stat.getHp_max() + stat.getAttack() + stat.getSpeed()/3.0f) * (1+stat.getDefense()));
     }
 
     public int getGoldValue() {
