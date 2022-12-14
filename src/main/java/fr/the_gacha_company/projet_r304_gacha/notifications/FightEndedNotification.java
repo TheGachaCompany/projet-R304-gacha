fa499c065fd87e4ccde158bf88414ee97e28ce86
Romain CLEMENT <rc2301@orange.fr>
2022-12-14 04:26:51 +0100

    Implemented thread to regen hp concurrently

--- src/main/java/fr/the_gacha_company/projet_r304_gacha/notifications/FightEndedNotification.java
+++
@@ -1,9 +0,0 @@
-package fr.the_gacha_company.projet_r304_gacha.notifications;
-
-public class FightEndedNotification extends Notification {
-
-    public FightEndedNotification() {
-        super(null);
-    }
-
-}
---
+++ src/main/java/fr/the_gacha_company/projet_r304_gacha/threads/notifications/FightEndedNotification.java
@@ -0,0 +1,9 @@
+package fr.the_gacha_company.projet_r304_gacha.threads.notifications;
+
+public class FightEndedNotification extends Notification {
+
+    public FightEndedNotification(NotificationManager manager) {
+        super(manager, null);
+    }
+
+}
