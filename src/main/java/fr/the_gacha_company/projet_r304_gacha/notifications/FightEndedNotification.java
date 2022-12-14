4337ac511bfdd5fa9b66e4a1350dbd379ea72f30
Romain CLEMENT <rc2301@orange.fr>
2022-12-14 03:16:30 +0100

    Created notifications classes

---
+++ src/main/java/fr/the_gacha_company/projet_r304_gacha/notifications/FightEndedNotification.java
@@ -0,0 +1,9 @@
+package fr.the_gacha_company.projet_r304_gacha.notifications;
+
+public class FightEndedNotification extends Notification {
+
+    public FightEndedNotification() {
+        super(null);
+    }
+
+}
---
+++ src/main/java/fr/the_gacha_company/projet_r304_gacha/notifications/Notification.java
@@ -0,0 +1,11 @@
+package fr.the_gacha_company.projet_r304_gacha.notifications;
+
+public abstract class Notification {
+
+    private final String message;
+
+    public Notification(String message) {
+        this.message = message;
+    }
+
+}
