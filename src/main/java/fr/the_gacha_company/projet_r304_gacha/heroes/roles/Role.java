76cceedaab56e287564b8c80085e2181673b1547
thibaultDP <thibault.DE-PERMENTIER@etu.univ-amu.fr>
2022-12-15 14:42:35 +0100

    comment on the roles

--- src/main/java/fr/the_gacha_company/projet_r304_gacha/heroes/roles/Role.java
+++ src/main/java/fr/the_gacha_company/projet_r304_gacha/heroes/roles/Role.java
@@ -3,12 +3,17 @@ package fr.the_gacha_company.projet_r304_gacha.heroes.roles;
 import fr.the_gacha_company.projet_r304_gacha.Global;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+/**
+ * Abstract class that represent role that Heroes and Monster can have
+ */
 public abstract class Role {
 
+    /**
+     * Get a random role within the physical and magical roles
+     * @return A random role
+     */
     public static Role getRandom() {
         List<Role> roles = new ArrayList<>();
         roles.addAll(PhysicalRole.getRoles());
@@ -20,18 +25,38 @@ public abstract class Role {
     private final String name;
     private final double[] efficiency;
 
+    /**
+     * Create a new role
+     * @param id The role id
+     * @param name The name of the role
+     * @param efficiency The efficiency the attack of this role have on other role store in an array of double
+     */
     public Role(int id, String name, double[]  efficiency) {
         this.id = id;
         this.name = name;
         this.efficiency = efficiency;
     }
 
+    /**
+     * Get the id of the role
+     * @return The id
+     */
     public int getId() {return id;}
 
+    /**
+     * Get the name of the role
+     * @return The name
+     */
     public String getName() {
         return name;
     }
 
+
+    /**
+     * Get the efficiency of the attack of this role
+     * @param id
+     * @return The efficiency
+     */
     public double getEfficency(int id) {
         return efficiency[id];
     }
